33a4b1c5bfe2709031e98f1e1aaa0b76552bb6b7
unknown <jsunj.jones@gmail.com>
2019-01-01 15:24:02 -0500

    add Add Two Numbers

---
+++ Add Two Numbers/README.md
@@ -0,0 +1,3 @@
+## Add Two Numbers
+
+A simple function that takes in two numbers and adds them together.
---
+++ Add Two Numbers/addtwonumbers.js
@@ -0,0 +1,5 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addTwoNumbers(1, 2)); // returns 3
