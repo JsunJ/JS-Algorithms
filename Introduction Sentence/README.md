de1ce16609c9aaa714c6320fb67ab8413097d3a5
unknown <jsunj.jones@gmail.com>
2019-01-01 23:55:40 -0500

    Add Introduction

---
+++ Introduction Sentence/README.md
@@ -0,0 +1,3 @@
+## Introduction Sentence
+
+A simple function that takes in a first name and last name then returns a brief introduction.
---
+++ Introduction Sentence/introduction.js
@@ -0,0 +1,6 @@
+function introduction(firstName, lastName) {
+  const introduction = `Hi, my name is ${firstName} ${lastName}.`;
+  return introduction;
+}
+
+console.log(introduction("John", "Doe")); // Hi, my name is John Doe.
