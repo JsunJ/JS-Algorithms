a03e0b0ec66432922647ec7b9accc925a46de7ca
unknown <jsunj.jones@gmail.com>
2019-01-04 22:17:54 -0500

    Add Reverse a String

---
+++ Reverse a String/README.md
@@ -0,0 +1,3 @@
+## Reverse a String
+
+A function that takes in a string and returns the reverse.
---
+++ Reverse a String/reverseString.js
@@ -0,0 +1,14 @@
+/* Decrementing for loop */
+
+function reverseString(string) {
+  let newString = "";
+
+  // Start at last character and iterate backwards, stop after first character
+  for (let i = string.length - 1; i >= 0; i--) {
+    // Add current character to new string
+    newString += string[i];
+  }
+  return newString;
+}
+
+console.log(reverseString("Jason"));
