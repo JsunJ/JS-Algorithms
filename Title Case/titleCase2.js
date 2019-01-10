b683c655d2d78cdec69803139b09ea606d28c57d
unknown <jsunj.jones@gmail.com>
2019-01-09 20:29:44 -0500

    Clean up

--- Title Case/titleCase2.js
+++ Title Case/titleCase2.js
@@ -1,9 +1,9 @@
 function titleCase(str) {
-  let newStr = str.toLowerCase().split(" ").map(function(word) {
-    return (word.charAt(0).toUpperCase() + word.slice(1));
-  }).join(" ");
-
-  return newStr;
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
 }
 
 console.log(titleCase("My name is Jason.")); // My Name Is Jason.
